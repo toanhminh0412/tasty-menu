bf741a64eb688de60d21e388dcfb589465415f6c
Archie To <toanhminh0412@gmail.com>
2024-02-19 14:03:48 -0800

    Change Login to Signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -81,9 +81,9 @@ export default function SignupPage() {
                             </div> */}
                             <Link href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</Link>
                         </div>
-                        <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800">Log in</button>
+                        <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800">Sign up</button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <Link href="/login" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Login</Link>
+                            Don&apos;t have an account yet? <Link href="/login" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Login</Link>
                         </p>
                     </form>
                 </div>
