58c1fff363f0ae4c053488776714425ed92807d4
Archie To <toanhminh0412@gmail.com>
2024-02-19 16:36:18 -0800

    Redirect to dashboard after logging in successfully

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,11 +23,10 @@ export default function LoginPage() {
 
         // Login successfully
         if (response.ok) {
-            console.log(document.cookie)
             console.log("Login successful");
             displaySuccessMessage("Login successful. Redirecting to dashboard...");
             // Redirect to homepage
-            // window.location.href = "/";
+            window.location.href = "/";
 
         // Something wrong with Django server
         } else if (response.status === 500) {
