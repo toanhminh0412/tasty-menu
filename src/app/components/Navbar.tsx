151564bff5c39628a5e5f36a3fb2ccdee9ffd68c
Archie To <toanhminh0412@gmail.com>
2024-02-27 17:17:51 -0800

    Remove a print statement

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,6 @@ import LogoutBtn from "./buttons/LogoutBtn";
 
 export default async function Navbar() {
     const session = await getServerSession();
-    console.log(session);
 
     return (
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
